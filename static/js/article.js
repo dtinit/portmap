8b4f9281c37259a3619dd394da6228c586cfae95
alexbainter <alexbainter@gmail.com>
2024-02-28 12:12:57 -0600

    Format files with prettier

--- static/js/article.js
+++ static/js/article.js
@@ -1,28 +1,28 @@
 /* global lucide */
 
 function lucideElement(name) {
-  const element = document.createElement("i");
-  element.setAttribute("data-lucide", name);
+  const element = document.createElement('i');
+  element.setAttribute('data-lucide', name);
   return element;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  let happyButton = document.getElementById("id_reaction_0");
-  let sadButton = document.getElementById("id_reaction_1");
+document.addEventListener('DOMContentLoaded', function () {
+  let happyButton = document.getElementById('id_reaction_0');
+  let sadButton = document.getElementById('id_reaction_1');
 
-  happyButton.nextSibling.replaceWith(lucideElement("smile")); // replaces "yes" string with smily face
-  sadButton.nextSibling.replaceWith(lucideElement("frown")); // replaces "no" with frown
+  happyButton.nextSibling.replaceWith(lucideElement('smile')); // replaces "yes" string with smily face
+  sadButton.nextSibling.replaceWith(lucideElement('frown')); // replaces "no" with frown
 
   lucide.createIcons();
 
-  let explanationText = document.getElementById("id_explanation");
+  let explanationText = document.getElementById('id_explanation');
   explanationText.hidden = true;
   let explanationLabel =
-    explanationText.parentElement.getElementsByTagName("label")[0];
-  explanationLabel.style.visibility = "hidden";
+    explanationText.parentElement.getElementsByTagName('label')[0];
+  explanationLabel.style.visibility = 'hidden';
 
-  sadButton.addEventListener("change", function () {
-    explanationLabel.style.visibility = "visible";
+  sadButton.addEventListener('change', function () {
+    explanationLabel.style.visibility = 'visible';
     explanationText.hidden = false;
   });
 });
