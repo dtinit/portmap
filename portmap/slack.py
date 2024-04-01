638de4c99c236d8728bee5f4bdbd585b63529d20
alexbainter <alexbainter@gmail.com>
2024-04-01 14:32:56 -0500

    Wrap slack notification attempt in try/except

--- portmap/slack.py
+++ portmap/slack.py
@@ -2,4 +2,7 @@ from django.conf import settings
 import requests
 
 def notify(text):
-    requests.post(settings.SLACK_NOTIFICATION_WEBHOOK_URL, json={'text':text})
+    try:
+        requests.post(settings.SLACK_NOTIFICATION_WEBHOOK_URL, json={'text':text})
+    except:
+        print("Slack notification failed. Is SLACK_NOTIFICATION_WEBHOOK_URL set correctly?")
