3cd17a97e61851d86a50fbbd01d71165dba538b8
alexbainter <alexbainter@gmail.com>
2024-04-09 12:20:59 -0500

    Use Django's built-in server support for E2E tests

--- tests/e2e/test_smoke.py
+++ tests/e2e/test_smoke.py
@@ -1,20 +1,36 @@
 import re
-from playwright.sync_api import Page, Route, expect
+import os
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from playwright.sync_api import expect, sync_playwright
+from portmap.core.articles import get_content_files
 
 # Smoke tests to make sure the site's primary functions are working
 
-def test_has_title(page: Page):
-    page.goto("http://localhost:8000")
+class SmokeTests(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+        super().setUpClass()
+        cls.playwright = sync_playwright().start()
+        cls.browser = cls.playwright.chromium.launch()
 
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("PortMap"))
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.browser.close()
+        cls.playwright.stop()
 
-def test_query(page: Page):
-    page.goto("http://localhost:8000")
+    def test_has_title(self):
+        page = self.browser.new_page()
+        page.goto(self.live_server_url)
+        expect(page).to_have_title(re.compile("PortMap"))
 
-    page.get_by_text('Contacts').click()
-    page.get_by_label('Transfer from').select_option('iCloud')
-    page.get_by_text('Find Article').click()
-
-    # This will break if the filename changes or another matching article is added
-    expect(page).to_have_url('http://localhost:8000/articles/contacts1.md/')
+    def test_query(self):
+        get_content_files()
+        page = self.browser.new_page()
+        page.goto(self.live_server_url)
+        page.get_by_text('Contacts').click()
+        page.get_by_label('Transfer from').select_option('iCloud')
+        page.get_by_text('Find Article').click()
+        # This will break if the filename changes or another matching article is added
+        expect(page).to_have_url(f"{self.live_server_url}/articles/contacts1.md/")
